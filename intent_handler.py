259c90f1f995b3566679002276c4aa3c7369e769
Jakub Kitka <jki@novelpay.pl>
2023-04-22 16:49:06 +0200

    feat: impl intent_handler

--- intent_handler.py
+++ intent_handler.py
@@ -1,9 +1,36 @@
 import signal
-import os
-import sys
- 
-n = int(sys.argv[1])
-print(n+1)
+import random
+import time
 
-#pid = input("Enter PID:")
-#os.kill(int(pid), signal.SIGUSR1)
+
+
+class Intent:
+    def __init__(self, h_src, h_dst, limit):
+        self.h_src = h_src
+        self.h_dst = h_dst
+        self.limit = limit
+    
+    def __str__(self):
+        return "Flow [H{}<->H{}, {}ms]".format(self.h_src, self.h_dst, self.limit)
+    
+def rand_source_host():
+    return random.randint(1,3)
+
+def rand_destination_host():
+    return random.randint(4,6)
+
+def rand_delay_value():
+    return random.randint(1,10)
+
+def termination_handler(signum, frame):
+    print ("Termination buttons clicked")
+    intent = Intent(rand_source_host(), rand_destination_host(), rand_delay_value())
+    print(intent)
+
+
+signal.signal(signal.SIGTSTP, termination_handler)
+
+while True:
+    print("Dzienciol sobie leci")
+    time.sleep(1)
+
